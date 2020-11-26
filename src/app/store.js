10673f5147b15df465236d304f9dc70c467adb60
yahiaouik <kenza.yahiaoui@telecom-st-etienne.fr>
2020-11-26 11:06:56 +0100

    remove counter exemple

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import callbotReducer from './callbotSlice';
 import callReducer from './callSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     callbot: callbotReducer,
     call: callReducer,
   },
